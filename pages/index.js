a912c3ecef1039adc634dec4a0ce1c99553a37a2
johnvtovar67-alt <johnvtovar67@gmail.com>
2026-04-19 12:25:42 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: 'Arial, sans-serif' }}>
+      <h1>Auto Quant Screener</h1>
+      <p>If you see this, your app is finally working.</p>
+    </main>
+  );
+}
