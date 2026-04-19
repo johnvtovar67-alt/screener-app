0daa67fcbec2382bcce6315cb63af619d72e835b
johnvtovar67-alt <johnvtovar67@gmail.com>
2026-04-19 14:35:49 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,1 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Auto Quant Screener</h1>
-      <p>If you see this, your app is working.</p>
-    </main>
-  );
-}
+
